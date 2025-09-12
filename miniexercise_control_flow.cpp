7e9ae93e474ddcc43e51d9954b2e796d89552aea
Ekene Ginikachi <ekeneginikachi16@gmail.com>
2025-09-12 12:30:06 +0100

    Second Phase: I used a while loop and a switch statement

--- miniexercise_control_flow.cpp
+++ miniexercise_control_flow.cpp
@@ -3,21 +3,43 @@
 
 int main(){
 
-    // To if the number is positive, negative or zero.
-    
+    // To check if a number is positive, negative or zero.
+
     double num;
 
     std::cout << "Please enter any number(be negative or positive)" << std::endl;
     std::cin >> num;
-    std::cout << std::endl;
-
+   
     if(num > 0){
-        std::cout << "The number you enter is positive." << std::endl; 
+        std::cout << "The number you entered is positive." << std::endl; 
     }else if(num <= -1){
-        std::cout << "The number you enter is negative." << std::endl;
+        std::cout << "The number you entered is negative." << std::endl;
     }else if(num == 0){
-        std::cout << "The number you enter is zero." << std::endl;
+        std::cout << "The number you entered is zero." << std::endl;
     }else{
         std::cout << "Please enter a valid number." << std::endl;
+    }
+    std::cout << std::endl;
+
+    // Number representing days of the week.
+
+    int week;
+
+    std::cout << "Please enter a number between 1-7(which will represent your favourite day of the week.)" << std::endl;
+    std::cin >> week;
+
+    while(week > 7 || week < 1){
+        std::cout << "The number should range from 1-7. Re-enter the number" << std::endl;
+        std::cin >> week;
+    }
+
+    switch(week){
+        case 1: std::cout << "Your favourite day of the week is Sunday." << std::endl; break;
+        case 2: std::cout << "Your favourite day of the week is Monday." << std::endl; break;
+        case 3: std::cout << "Your favourite day of the week is Tuesday." << std::endl; break;
+        case 4: std::cout << "Your favourite day of the week is Wednesday." << std::endl; break;
+        case 5: std::cout << "Your favourite day of the week is Thursday." << std::endl; break;
+        case 6: std::cout << "Your favourite day of the week is Friday." << std::endl; break;
+        case 7: std::cout << "Your favourite day of the week is Saturday." << std::endl; break;
     }
 }
